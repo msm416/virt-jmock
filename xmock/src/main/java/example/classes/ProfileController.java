76f259f5d1e907af4bc216fc132bcf83e188ac46
msm416-laptop <msm416@ic.ac.uk>
2020-02-10 21:33:15 +0000

    Make pipeline pass (don't sleep for 2 seconds 1000 times...)

--- xmock/src/main/java/example/classes/ProfileController.java
+++ xmock/src/main/java/example/classes/ProfileController.java
@@ -14,11 +14,11 @@ public class ProfileController {
 
     public void lookUpFriends(long userId) {
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
 
         List<Long> friendIds = socialGraph.query(userId);
