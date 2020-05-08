b057e07a4a6d2d11345629734818c4e201578e43
msm416-laptop <msm416@ic.ac.uk>
2020-05-08 05:59:38 +0300

    remove obsolete method. It exists in other repo.

--- jmock/src/main/java/org/jmock/utils/LogsAndDistr.java
+++ jmock/src/main/java/org/jmock/utils/LogsAndDistr.java
@@ -172,65 +172,6 @@ public class LogsAndDistr {
         }
     }
 
-    public static void createLogFile() {
-        try {
-            Process process = Runtime.getRuntime().exec("heroku logs -n 1500");
-
-            StringBuilder output = new StringBuilder();
-
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                output.append(line + "\n");
-            }
-
-            int exitVal = process.waitFor();
-            if (exitVal == 0) {
-                Map<String, ArrayList<Integer>> methodToExecTimes = new HashMap<>();
-                Pattern pattern = Pattern.compile("EXECTIME\\(ms\\)\\sfor\\s(?<method>.*\\(\\))\\s=\\s(?<time>\\d+)");
-                Matcher matcher = pattern.matcher(output);
-                while (matcher.find()) {
-                    String method = matcher.group("method");
-                    Integer execTime = Integer.parseInt(matcher.group("time"));
-                    if (methodToExecTimes.containsKey(method)) {
-                        methodToExecTimes.get(method).add(execTime);
-                    } else {
-                        methodToExecTimes.put(method, new ArrayList() {{
-                            add(execTime);
-                        }});
-                    }
-                }
-                File logs = new File("logs.txt");
-                if (logs.createNewFile()) {
-                    System.out.println("Log file created: " + logs.getName());
-                } else {
-                    System.out.println("Log file already exists. Will proceed to overwrite it.");
-                }
-                FileWriter myWriter = new FileWriter(logs.getName());
-                for (Map.Entry<String, ArrayList<Integer>> entry : methodToExecTimes.entrySet()) {
-                    myWriter.write(entry.getKey() + ":" + "[");
-                    ArrayList<Integer> execTimes = entry.getValue();
-                    int i;
-                    for (i = 0; i < execTimes.size() - 1; i++) {
-                        myWriter.write(execTimes.get(i) + ",");
-                    }
-                    myWriter.write(execTimes.get(i) + "");
-                    myWriter.write("]\n");
-                }
-                //System.out.println(output);
-                myWriter.close();
-                System.out.println("SUCCESFULLY GENNERATED LOG FILES.");
-                System.exit(0);
-            } else {
-                System.out.println("SOMETHING BAD HAPPENED");
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static ArrayList<Double> getSamplesFromLog(String logfile, String methodName) {
         ArrayList<Double> samples = new ArrayList<>();
         String logfileContent = null;
